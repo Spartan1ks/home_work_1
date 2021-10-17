497978605a3b1e4bbd002d165ddd9745779a3eb3
spartaniks <donyevich@gmail.com>
2021-10-17 14:27:21 +0300

    Create hw_1.py

---
+++ hw_1.py
@@ -0,0 +1,48 @@
+first = 10
+second = 30
+third = first + second
+print(third)
+
+first = 10
+second = 30
+third = first - second
+print(third)
+
+first = 10
+second = 30
+third = first * second
+print(third)
+
+first = 10
+second = 30
+third = first ** second
+print(third)
+
+first = 10
+second = 30
+third = first / second
+print(third)
+
+first = 10
+second = 30
+third = first // second
+print(third)
+
+first = 10
+second = 30
+third = first % second
+print(third)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
